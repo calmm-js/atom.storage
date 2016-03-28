4c43ceb971cdf23d8389e264f199aaa555376822
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-03-28 19:59:50 +0300

    Started readme

---
+++ README.js
@@ -0,0 +1,4 @@
+Minimalistic storage (e.g. localStorage) implementation for Atoms.
+
+[![npm version](https://badge.fury.io/js/atom.storage.svg)](http://badge.fury.io/js/atom.storage) [![Build Status](https://travis-ci.org/calmm-js/atom.storage.svg?branch=master)](https://travis-ci.org/calmm-js/atom.storage) [![](https://david-dm.org/calmm-js/atom.storage.svg)](https://david-dm.org/calmm-js/atom.storage) [![](https://david-dm.org/calmm-js/atom.storage/dev-status.svg)](https://david-dm.org/calmm-js/atom.storage#info=devDependencies) [![Gitter](https://img.shields.io/gitter/room/calmm-js/chat.js.svg?style=flat-square)](https://gitter.im/calmm-js/chat)
+
